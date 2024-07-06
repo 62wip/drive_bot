4a28a084c3f69cdb51bd6e317a29b484c8437479
62wip <147309505+62wip@users.noreply.github.com>
2024-07-07 00:48:26 +0300

    Create cfg-EXAMPLE.py

---
+++ cfg-EXAMPLE.py
@@ -0,0 +1,7 @@
+token = 'YOUR_API_TOKEN'
+number = 'YOUR_NUMBER'
+api = 'YOUR_API'
+client_id = 'YOUR_CLIENT_ID'
+redirect_url = 'https://t.me/wf_drive_bot'
+
+don_token = 'YOUR_DONAT_TOKEN_FROM_U_MONEY'
